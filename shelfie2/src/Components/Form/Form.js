4db7f8224ca94c4a96381f66e3ad8bce187c5a83
DogSniffers <DogSniffers@users.noreply.github.com>
2020-02-16 16:20:35 -0700

    Form part1

--- shelfie2/src/Components/Form/Form.js
+++ shelfie2/src/Components/Form/Form.js
@@ -3,11 +3,38 @@ import React, {Component} from 'react';
 export default class Form extends Component{
     constructor(){
         super()
+        this.state = {
+            imageUrl:'',
+            productName:'',
+            price:0,
+        }
+    }
+    handleChangeImageUrl= value =>{
+        this.setState({imageUrl:value})
+    }
+
+    handleChangeProductName= value =>{
+        this.setState({productName:value})
+    }
+
+    handleChangePrice= value =>{
+        this.setState({price:value})
+    }
+
+    cancelButton= () =>{
+        this.setState({imageUrl:'',productName:'',price:0})
     }
 
     render(){
+        console.log(this.state)
         return(
-            <div>Form</div>
+            <div>
+                <input onChange={(e) => this.handleChangeImageUrl(e.target.value)}></input>
+                <input onChange={(e) => this.handleChangeProductName(e.target.value)}></input>
+                <input onChange={(e) => this.handleChangePrice(e.target.value)}></input>
+                <button onClick={() => this.cancelButton()}>Cancel</button>
+                <button>Add to Inventory</button>
+            </div>
         )
     }
 }
