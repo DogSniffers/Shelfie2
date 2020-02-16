dab7e626b648069716f99d4012bbf1e1ad094b7d
DogSniffers <DogSniffers@users.noreply.github.com>
2020-02-16 15:56:29 -0700

    Setup fully setup

--- shelfie2/server/index.js
+++ shelfie2/server/index.js
@@ -1,5 +1,22 @@
+require('dotenv').config();
+const {CONNECTION_STRING, SERVER_PORT} = process.env
 const express = require('express');
+const massive = require('massive');
 const app = express();
 const controller = require('./controller');
 
 app.use(express.json())
+
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl:{
+        rejectUnauthorized: false
+    }
+}).then(dbInstance =>{
+    app.set('db',dbInstance);
+    console.log('Database connected');
+    app.listen(SERVER_PORT, ()=>
+    console.log(`Listening on Port ${SERVER_PORT}`));
+    }
+)
